bf9ed503b24e0a4ff2dc16ce9048842d8fa2d734
Christelle Nirere <mariechristellenirere@gmail.com>
2023-03-23 17:45:32 +0200

    Design desktop version

--- speakers.js
+++ speakers.js
@@ -23,41 +23,41 @@ const content = [
         id: 3,
         name: 'Masashi Sugiyama',
         Image: {
-            img: "images/speakers/Masashi1.jpg",
+            img: "images/speakers/sugiyama2.jpeg",
             alt: 'speaker 3',
         },
         job: 'prominent Japanese computer scientist and artificial intelligence (AI) researcher',
-        about: 'a professor at the University of Tokyo and the director of the RIKEN Center for Advanced Intelligence Project'
+        about: 'a professor at the University of Tokyo and the director of the RIKEN Center for AI .'
     },
     {
         id: 4,
         name: 'Girmaw Abebe Tadesse',
         Image: {
-            img: "images/speakers/abebe1.jpeg",
+            img: "images/speakers/girmaw_abebe22.jpg",
             alt: "speaker 4",
         },
         job: 'computer scientist and educator',
-        about: 'currently a lecturer and researcher at Addis Ababa University in Ethiopia, where he teaches courses on topics such as computer networking, computer security, and mobile computing.',
+        about: 'researcher at Addis Ababa University in Ethiopia with successfull collaboration',
     },
     {
         id: 5,
         name: 'Jascha Sohl-Dickstein',
         Image: {
-            img: "images/speakers/Jascha1.png",
+            img: "images/speakers/jasack22.jpg",
             alt: 'speaker 5',
         },
         job: 'research scientist and engineer',
-        about: 'currently a research scientist at Google Brain, where he works on developing new algorithms and techniques for deep learning.',
+        about: 'a scientist at Google Brain,works on developing new algorithms for deep learning.',
     },
     {
         id: 6,
         name: 'Dilek Hakkani-Tur',
         Image: {
-            img: "images/speakers/dilek1.jpeg",
+            img: "images/speakers/Dilek_Portrait-22.jpg",
             alt: "speaker 6",
         },
         job: ' prominent researcher and scientist in the field of artificial intelligence and natural language processing',
-        about: 'currently a Principal Research Scientist at Amazon Web Services (AWS), where she leads the Conversational AI team.',
+        about: 'Research Scientist at Amazon Web Services, she leads the Conversational AI team.',
     }
 
 ];
@@ -82,7 +82,8 @@ content.forEach((info) => {
   container.appendChild(card);
 });
 
-const teamlist = document.querySelectorAll('#presenters .other-speakers li');
+
+const teamlist = document.querySelectorAll('.cards');
 const MoreButt = document.querySelector('#presenters button');
 const SpanButt = document.querySelector('#presenters button span');
 const IconButt = document.querySelector('#presenters button i');
@@ -91,8 +92,10 @@ let stat = false;
 
 function displayMore() {
   if (!stat) {
-    teamlist.forEach((team) => {
-      team.style.display = 'flex';
+    teamlist.forEach((team, index) => {
+      if (index >= 2) {
+        team.style.display = 'flex';
+      }
     });
     SpanButt.textContent = 'LESS';
     IconButt.classList.remove('fa-angle-down');
@@ -100,7 +103,7 @@ function displayMore() {
     stat = true;
   } else {
     teamlist.forEach((team, index) => {
-      if (index > 1) {
+      if (index >= 2) {
         team.style.display = 'none';
       }
     });
@@ -110,6 +113,14 @@ function displayMore() {
     stat = false;
   }
 }
+
+// Hide all speakers except the first two by default
+teamlist.forEach((team, index) => {
+  if (index >= 2) {
+    team.style.display = 'none';
+  }
+});
+
 
 MoreButt.addEventListener('click', displayMore);
 
